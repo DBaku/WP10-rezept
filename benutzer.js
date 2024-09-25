5fd95c961880e4cd6c1066c48a9a77aec00e31a4
DBaku <149767371+DBaku@users.noreply.github.com>
2024-09-25 10:15:18 +0200

    .

---
+++ benutzer.js
@@ -0,0 +1,9 @@
+db.benutzer.insertOne({
+    name: "Max Mustermann",
+    profiltext: "Ich liebe kochen!",
+    geburtstag: "1990-04-15",
+    links: { facebook: "https://facebook.com/max", instagram: "https://instagram.com/max" },
+    followers: [],
+    following: [],
+    rezeptbuch: [],
+});
